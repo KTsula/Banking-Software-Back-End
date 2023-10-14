9cc454428f9a1cdb55a5ae26afe6d0985aa0388a
Keti Sula <ketisulamanidze@gmail.com>
2023-10-14 19:33:27 +0200

    Added 1 test case

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -34,11 +34,22 @@ def test_create_account(testing_client):
     )
     assert response.status_code == 200
 
+def test_accountWithTooLongName_notCreate(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts' page is posted to (POST)
+    THEN check the response is not valid
+    """
+    response = testing_client.post(
+        "/accounts", json={"name": "John Doeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", "country": "Spain", "currency": "€"}
+    )
+    assert response.status_code == 400
+
 
 def test_delete_account(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts' page is deleted (DELETE)
+    WHEN the '/accounts' page posted to delete (DELETE)
     THEN check the response is valid
     """
 
@@ -47,6 +58,7 @@ def test_delete_account(testing_client):
         "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
     )
 
+    # Delete the account by id
     response = testing_client.delete("/accounts/1")
     assert response.status_code == 200
 
@@ -58,10 +70,11 @@ def test_get_account_by_id(testing_client):
     THEN check the response is valid
     """
 
-    # Create an account first
+    # Create an account 
     response = testing_client.post(
         "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
     )
 
+    # Get the account by id
     response = testing_client.get("/accounts/1")
     assert response.status_code == 200
